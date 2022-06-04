6ebe35d48ee9cb1803e24dd41512d532a9bd4caa
Frank Kulak <fs.kulak@gmail.com>
2022-06-04 15:53:35 -0400

    Update build-maps.ts

---
+++ src/functions/build-maps.ts
@@ -0,0 +1,54 @@
+import type { CombinedTuningResource, StringTableResource } from "@s4tk/models";
+import { Package } from "@s4tk/models";
+import { formatStringKey } from "@s4tk/hashing/formatting";
+import type { FileMap, SimulationIndex } from "../types";
+
+/**
+ * Creates a map from tuning IDs (as decimal strings) to their file names.
+ * 
+ * @param index Simulation file index to base map on
+ */
+export function buildSimulationMap(
+  index: SimulationIndex
+): Map<string, string> {
+  const map = new Map<string, string>();
+
+  index.combined.forEach((positions, filepath) => {
+    const cbt = Package.fetchResources<CombinedTuningResource>(filepath, positions);
+
+    cbt.forEach(({ value }) => {
+      const rNodes = value.dom.child.children.filter(node => node.tag === "R");
+
+      rNodes.forEach(rNode => {
+        rNode.children.forEach(tuningNode => {
+          map.set(tuningNode.id as string, tuningNode.name);
+        });
+      });
+    });
+  });
+
+  return map;
+}
+
+/**
+ * Creates a map from string keys (as hex strings) to their actual values.
+ * 
+ * @param index String table file index to base map on
+ */
+export function buildStringTableMap(
+  index: FileMap
+): Map<string, string> {
+  const map = new Map<string, string>();
+
+  index.forEach((positions, filepath) => {
+    const stbls = Package.fetchResources<StringTableResource>(filepath, positions);
+
+    stbls.forEach(({ value }) => {
+      value.entries.forEach(stringEntry => {
+        map.set(formatStringKey(stringEntry.key), stringEntry.value);
+      });
+    });
+  });
+
+  return map;
+}
