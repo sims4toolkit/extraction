3f44916e75271921d16c6b818838d8a9669bda22
Frank Kulak <fs.kulak@gmail.com>
2022-06-04 18:23:29 -0400

    Use options in file extraction.

--- src/lib/extract-files.ts
+++ src/lib/extract-files.ts
@@ -7,10 +7,7 @@ import type { ResourceKeyPair } from "@s4tk/models/types";
 import { locateSimulationPackages, locateStringTablePackages } from "./locate-packages";
 import { indexSimulationPackages, indexStringTablePackages } from "./index-packages";
 import { buildSimulationMap, buildStringTableMap } from "./build-maps";
-import { ExtractionOptions } from "./types";
-
-// FIXME: delete
-const TEMPORARY_USER_LOCALE = 0;
+import { ExtractionOptions, setDefaultOptions } from "./options";
 
 /**
  * Extracts all tuning and SimData files from the packages in the given
@@ -25,36 +22,49 @@ export function extractFiles(
   outDir: string,
   options?: ExtractionOptions
 ) {
+  options = setDefaultOptions(options);
+
   // creating stbl index
-  const stblPaths = locateStringTablePackages(TEMPORARY_USER_LOCALE, srcDirs);
-  const stblIndex = indexStringTablePackages(stblPaths);
+  if (options.restoreComments) {
+    const stblPaths = locateStringTablePackages(options.targetLocale, srcDirs);
+    var stblIndex = indexStringTablePackages(stblPaths);
+  }
 
   // creating simulation index
-  const simPaths = locateSimulationPackages(srcDirs);
-  const simIndex = indexSimulationPackages(simPaths);
+  if (options.extractTuning || options.extractSimData) {
+    var simPaths = locateSimulationPackages(srcDirs);
+    var simIndex = indexSimulationPackages(simPaths);
+  }
 
   // building comment map
-  const commentMap = new Map<string, string>();
-  buildStringTableMap(stblIndex, commentMap);
-  buildSimulationMap(simIndex, commentMap);
+  let commentMap: Map<string, string>;
+  if (options.restoreComments) {
+    commentMap = new Map();
+    buildStringTableMap(stblIndex, commentMap);
+    buildSimulationMap(simIndex, commentMap);
+  }
 
   // extracting tuning
-  simIndex.combined.forEach((positions, filepath) => {
-    const cbt = Package.fetchResources<CombinedTuningResource>(filepath, positions);
-    cbt.forEach(entry => {
-      entry.value.toTuning({ commentMap }).forEach(tuning => {
-        writeTuningFile(outDir, entry.key.group, tuning);
+  if (options.extractTuning) {
+    simIndex.combined.forEach((positions, filepath) => {
+      const cbt = Package.fetchResources<CombinedTuningResource>(filepath, positions);
+      cbt.forEach(entry => {
+        entry.value.toTuning({ commentMap }).forEach(tuning => {
+          writeTuningFile(outDir, entry.key.group, tuning, options);
+        });
       });
     });
-  });
+  }
 
   // extracting simdata
-  simIndex.simdata.forEach((positions, filepath) => {
-    const simdatas = Package.fetchResources<SimDataResource>(filepath, positions);
-    simdatas.forEach(entry => {
-      writeSimDataFile(outDir, entry);
+  if (options.extractSimData) {
+    simIndex.simdata.forEach((positions, filepath) => {
+      const simdatas = Package.fetchResources<SimDataResource>(filepath, positions);
+      simdatas.forEach(entry => {
+        writeSimDataFile(outDir, entry, options);
+      });
     });
-  });
+  }
 }
 
 /**
@@ -63,15 +73,23 @@ export function extractFiles(
  * @param outDir Directory to output this file to
  * @param group Group of combined tuning that this tuning was found in
  * @param tuning Tuning file to write
+ * @param options User options
  */
-function writeTuningFile(outDir: string, group: number, tuning: XmlResource) {
+function writeTuningFile(
+  outDir: string,
+  group: number,
+  tuning: XmlResource,
+  options: ExtractionOptions
+) {
   const type = TuningResourceType.parseAttr(tuning.root.attributes.i);
   const instance = BigInt(tuning.root.id);
   const key = formatResourceTGI(type, group, instance, "!");
   const filename = `${key}.${tuning.root.name}.xml`;
-  const typeFolder = TuningResourceType[type] ?? "Unknown";
-  const classFolder = tuning.root.attributes.c ?? "Unknown";
-  const subfolders = path.join(outDir, typeFolder, classFolder);
+  let subfolders = outDir;
+  if (options.usePrimarySubfolders)
+    subfolders = path.join(subfolders, TuningResourceType[type] ?? "Unknown");
+  if (options.useSecondarySubfolders)
+    subfolders = path.join(subfolders, tuning.root.attributes.c ?? "Unknown");
   if (!fs.existsSync(subfolders)) fs.mkdirSync(subfolders, { recursive: true });
   const filepath = path.join(subfolders, filename);
   fs.writeFileSync(filepath, tuning.getBuffer());
@@ -82,12 +100,20 @@ function writeTuningFile(outDir: string, group: number, tuning: XmlResource) {
  * 
  * @param outDir Directory to output this file to
  * @param simdata SimData entry to write
+ * @param options User options
  */
-function writeSimDataFile(outDir: string, simdata: ResourceKeyPair<SimDataResource>) {
+function writeSimDataFile(
+  outDir: string,
+  simdata: ResourceKeyPair<SimDataResource>,
+  options: ExtractionOptions
+) {
   const key = formatResourceKey(simdata.key);
   const filename = `${key}.${simdata.value.instance.name}.xml`;
-  const groupFolder = SimDataGroup[simdata.key.group] ?? "Unknown";
-  const subfolders = path.join(outDir, "SimData", groupFolder);
+  let subfolders = outDir;
+  if (options.usePrimarySubfolders)
+    subfolders = path.join(subfolders, "SimData");
+  if (options.useSecondarySubfolders)
+    subfolders = path.join(subfolders, SimDataGroup[simdata.key.group] ?? "Unknown");
   if (!fs.existsSync(subfolders)) fs.mkdirSync(subfolders, { recursive: true });
   const filepath = path.join(subfolders, filename);
   fs.writeFileSync(filepath, simdata.value.toXmlDocument().toXml());
---
+++ src/lib/options.ts
@@ -0,0 +1,63 @@
+import { StringTableLocale } from "@s4tk/models/enums";
+
+/**
+ * Optional arguments for extracting tuning.
+ */
+export interface ExtractionOptions extends
+  Partial<{
+    /**
+     * Whether or not SimData should be extracted. True by default.
+     */
+    extractSimData: boolean;
+
+    /**
+     * Whether or not tuning should be extracted from combined tuning. True by
+     * default.
+     */
+    extractTuning: boolean;
+
+    /**
+     * Whether or not comments should be added for string keys and tuning IDs.
+     * True by default.
+     */
+    restoreComments: boolean;
+
+    /**
+     * If restoring comments, this determines which language is used to restore
+     * string comments. English by default.
+     */
+    targetLocale: StringTableLocale;
+
+    /**
+     * Whether or not to use primary subfolders when writing the files. This 
+     * means tuning type for tuning files, and "SimData" for SimData files.
+     * True by default.
+     */
+    usePrimarySubfolders: boolean;
+
+    /**
+     * Whether or not to use secondary subfolders when writing the files. This 
+     * means class for tuning files, and group for SimData files. True by
+     * default.
+     */
+    useSecondarySubfolders: boolean;
+  }> { }
+
+/**
+ * Sets default values for options if they are undefined or null. Returns a new
+ * object so that the one given by the user isn't mutated.
+ * 
+ * @param options User-entered options
+ */
+export function setDefaultOptions(
+  options?: ExtractionOptions
+): ExtractionOptions {
+  return {
+    extractSimData: options?.extractSimData ?? true,
+    extractTuning: options?.extractTuning ?? true,
+    restoreComments: options?.restoreComments ?? true,
+    targetLocale: options?.targetLocale ?? StringTableLocale.English,
+    usePrimarySubfolders: options?.usePrimarySubfolders ?? true,
+    useSecondarySubfolders: options?.useSecondarySubfolders ?? true,
+  };
+}
