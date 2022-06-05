ab48d4a623b4b3c2bf9d52b284e1095ea3cd85b8
Frank Kulak <fs.kulak@gmail.com>
2022-06-04 21:18:16 -0400

    Update extraction.ts

--- src/extraction.ts
+++ src/extraction.ts
@@ -1,5 +1,11 @@
 import { extractFiles } from "./lib/extract-files";
+import type { ManifestType, ExtractionOptions } from "./lib/options";
+import type { ExtractionEvent } from "./lib/types";
 
 export {
-  extractFiles
+  extractFiles,
+  // Types
+  ExtractionEvent,
+  ExtractionOptions,
+  ManifestType,
 };
