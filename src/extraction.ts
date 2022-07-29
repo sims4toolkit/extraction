35c30cf538e3f1d250c2cbfbe8bdeaaad101c2d0
Frank Kulak <fs.kulak@gmail.com>
2022-07-29 10:50:39 -0400

    Use BufferFromFile plugin and update version.

--- src/extraction.ts
+++ src/extraction.ts
@@ -1,6 +1,10 @@
+import { registerPlugin } from "@s4tk/models/plugins";
+import BufferFromFile from "@s4tk/plugin-bufferfromfile";
 import { extractFiles } from "./lib/extract-files";
 import { loadStringMap, loadTuningMap } from "./lib/memory-loaders";
 import type { ExtractionOptions } from "./lib/options";
+
+registerPlugin(BufferFromFile);
 
 export {
   extractFiles,
